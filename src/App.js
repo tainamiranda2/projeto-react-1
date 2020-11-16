e15d1d92f659b839b0acc2cf7b6d68d02c78035f
tainamiranda2 <tainamirandaferreira@gmail.com>
2020-11-16 14:48:47 -0300

    Primeiro projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import logo from './logo.jpg';
+//import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+const [showImage, setShowImage] = useState(false);
+
+const toggle = () => {
+  setShowImage(!showImage);
+}
+
+return (
+<div className="App">
+<header className="App-header">
+<button type="button" onClick={toggle}>
+
+  {this.state.showImage ? 'Esconder' : 'Mostrar'}
+  </button>
+
+{this.state.showImage && (
+<img src={logo} className="App-logo" alt="logo" />
+)}
+  
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+    >
+    Teste
+  </a>
+</header>
+</div>
+);
+    
 }
 
 export default App;
