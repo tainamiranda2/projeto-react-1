4a23ab7a5de47c3c93dab45ea089f1a2963c9582
tainamiranda2 <tainamirandaferreira@gmail.com>
2020-11-23 15:05:17 -0300

    atualizando

--- web/src/pages/Root.js
+++ web/src/pages/Root.js
@@ -3,20 +3,25 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route, 
-    Link
+    Route
 }
 
 from 'react-router-dom';
 import PagesPromotionSeach from './Promotion/Search/Search';
+import PagesPromotionForm from './Promotion/form/form';
 
 const Root = () => {
     return(
         <Router>
             <Switch>
+
+            <Route path="/create" component={PagesPromotionForm}/>
+            <Route path="/edit/:id" component={PagesPromotionForm}/>
             <Route path="/" component={PagesPromotionSeach}/>
+
             </Switch>
         </Router>
     )
 }
+
 export default Root;
